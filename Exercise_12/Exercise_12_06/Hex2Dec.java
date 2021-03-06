f2a4d93a4e0d6e7d4ba6eab5698ec72600547016
Abdus Salam Laskar <52828944+Sajad-Lx@users.noreply.github.com>
2021-03-07 00:06:36 +0530

    Update Hex2Dec.java
    Changed if condition in line 34-35

--- Exercise_12/Exercise_12_06/Hex2Dec.java
+++ Exercise_12/Exercise_12_06/Hex2Dec.java
@@ -31,8 +31,8 @@ public class Hex2Dec {
 	public static int hexToDecimal(String hex) throws NumberFormatException {
 		int decimalValue = 0;
 		for (int i = 0; i < hex.length(); i++) {
-			if (!(hex.charAt(i) >= '0' && hex.charAt(i) <= '9') ||
-				 !(hex.charAt(i) >= 'A' && hex.charAt(i) <= 'F'))
+			if (!((hex.charAt(i) >= '0' && hex.charAt(i) <= '9') ||
+				 (hex.charAt(i) >= 'A' && hex.charAt(i) <= 'F')))
 				throw new NumberFormatException("String is not a hex string");
 			char hexChar = hex.charAt(i);
 			decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
